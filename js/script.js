d7ed26fb4f229171b67b748499f7957680b64d82
Brenda <brendateruel@aol.com>
2011-02-06 20:53:52 -0600

    modifying css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 ﻿$(document).ready(function() {
 	$('#tabs').tabs();
-	$('dd#palette1').hide();
+	$('dd#swatches1').hide();
 $('dt#palette1').click(function() {
-	$('dd#palette1').slideToggle('3000');
+	$('dd#swatches1').slideToggle('3000');
+});	
+	$('dd#swatches2').hide();
+$('dt#palette2').click(function() {
+	$('dd#swatches2').slideToggle('3000');
+});	
+	$('dd#swatches3').hide();
+$('dt#palette3').click(function() {
+	$('dd#swatches3').slideToggle('3000');
 });	
 });	
 
