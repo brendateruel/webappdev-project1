5f3d88891d0f79e7ebb38785757031fe5e5508a4
Brenda <brendateruel@aol.com>
2011-02-10 22:06:23 -0600

    updated header, added daily find, added tabs content, correct tab colors

--- js/script.js
+++ js/script.js
@@ -12,6 +12,18 @@ $('dt#palette2').click(function() {
 $('dt#palette3').click(function() {
 	$('dd#swatches3').slideToggle('3000');
 });	
+	$('dd#fonts1').hide();
+$('dt#type-style1').click(function() {
+	$('dd#fonts1').slideToggle('3000');
+});	
+	$('dd#fonts2').hide();
+$('dt#type-style2').click(function() {
+	$('dd#fonts2').slideToggle('3000');
+});	
+	$('dd#fonts3').hide();
+$('dt#type-style3').click(function() {
+	$('dd#fonts3').slideToggle('3000');
+});	
 });	
 
 	
